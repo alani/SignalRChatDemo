a2ac6eff3e8b843ab25324a94c75bc89a8d397f5
Ahmed Alani <ahmed.k.alani@gmail.com>
2012-10-30 22:45:47 -0400

    Improve PersistentConnection sample.

--- src/ChatPersistentConnection.cs
+++ src/ChatPersistentConnection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using SignalR;
+using Newtonsoft.Json;
 
 namespace SignalRChatDemo
 {
@@ -14,9 +15,31 @@ namespace SignalRChatDemo
 
         protected override Task OnReceivedAsync(IRequest request, string connectionId, string data)
         {
-            Connection.Broadcast("Connection" + connectionId + ":\t" + data);
+            var message = JsonConvert.DeserializeObject<ChatMessage>(data);
+
+            switch(message.Type)
+            {
+                case MessageType.Standard:
+                    Connection.Broadcast("Connection " + connectionId + ":\t" + message.Data);
+                    break;
+                case MessageType.Broadcast:
+                    Connection.Broadcast("From SERVER:  Ping Initiated by " + connectionId);
+                    break;
+            }
 
             return base.OnReceivedAsync(request, connectionId, data);
         }
+    }
+
+    public enum MessageType
+    {
+        Standard,
+        Broadcast
+    }
+
+    public class ChatMessage
+    {
+        public MessageType Type { get; set; }
+        public string Data { get; set; }
     }
 }
