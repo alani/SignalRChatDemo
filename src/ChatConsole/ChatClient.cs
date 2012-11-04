18077d9c7a3d41b6c4015653f86a45a6015766c7
Ahmed Alani <ahmed.k.alani@gmail.com>
2012-11-04 00:40:15 -0400

    Added console chat client.

---
+++ src/ChatConsole/ChatClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SignalR.Client.Hubs;
+
+namespace ChatConsole
+{
+    public class ChatClient
+    {
+        static void Main(string[] args)
+        {
+            // Create connection to hub
+            var hubConnection = new HubConnection("http://localhost:1116/");
+
+            // Create hub proxy
+            var chatProxy = hubConnection.CreateProxy("chatCentral");
+
+            // On event addMessage, print to Console
+            chatProxy.On<string>("addMessage", data => Console.WriteLine(data));
+
+            // Start connection
+            hubConnection.Start().Wait();
+
+            // Keep console open and accept user input
+            string line = null;
+            while ((line = Console.ReadLine()) != null)
+            {
+                // Send a message to the server
+                chatProxy.Invoke("SendMessage", line).Wait();
+            }
+        }
+    }
+}
